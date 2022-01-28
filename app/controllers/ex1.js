ee10dd192a4c5afa0cb3929d6a2b0f0424921197
= <22002748@etu.unicaen.fr>
2022-01-28 11:56:01 +0100

    TD1 End of session Push, adding Bootstrap and some code

--- app/controllers/ex1.js
+++ app/controllers/ex1.js
@@ -1,3 +1,32 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
-export default class Ex1Controller extends Controller {}
+export default class Ex1Controller extends Controller {
+  @tracked content = 'Entrez votre texte';
+  @tracked info = '';
+  MAX = 100;
+
+  get size() {
+    return 100 - this.content.length;
+  }
+
+  get style() {
+    if (this.size() < 50) {
+      if (this.size() < 20) {
+        return 'danger';
+      }
+      return 'warning';
+    }
+    return null;
+  }
+
+  get info() {
+    return false;
+  }
+
+  @action
+  test() {
+    this.content = 'test action avec les trwaptipwin';
+  }
+}
