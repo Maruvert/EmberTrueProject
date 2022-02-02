517882e3abb12f51cd32aa596e6573d13269ce86
= <22002748@etu.unicaen.fr>
2022-02-02 16:34:01 +0100

    TD2 2nd Push, creating methods in product-service.js and ex2 file

---
+++ app/routes/ex2.js
@@ -0,0 +1,11 @@
+import Route from '@ember/routing/route';
+import { service } from '@ember/service';
+
+export default class Ex2Route extends Route {
+  @service productServices;
+
+  toggleActive(service) {
+
+  }
+
+}
--- app/services/product-services.js
+++ app/services/product-services.js
@@ -2,7 +2,25 @@ import Service from '@ember/service';
 import { dProducts, dPromos } from 'EmberTrueProject/data/datas';
 
 export default class ProductServicesService extends Service {
+  products = dProducts;
+  promos = dPromos;
 
+  get activeServices() {
+    return this.products.filterBy((word) => word == 'true');
+  }
 
+  get countActive() {
+    return this.activeServices.length;
+  }
 
+  get sumActive() {
+    return this.activeServices.reduce((sum, product) => sum + product.price, 0);
+    /* <=>
+    let sum = 0;
+    for (int i = 0 ; i < this.activeServices.length ; i++) {
+      sum += this.activeServices[i].price;
+    }
+    return sum;
+     */
+  }
 }
