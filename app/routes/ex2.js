3da7a07069c0f4145e1d8a7e596c677a4b36ee4c
= <22002748@etu.unicaen.fr>
2022-02-02 16:55:33 +0100

    TD2 Final session Push, finishing ex2 class

--- app/routes/ex2.js
+++ app/routes/ex2.js
@@ -1,11 +1,13 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
+import { action, set } from '@ember/object';
 
 export default class Ex2Route extends Route {
   @service productServices;
-
-  toggleActive(service) {
-
+  model() {
+    return this.productServices;
   }
-
+  @action toggleActive(service) {
+    set(service, 'active', !service.active);
+  }
 }
